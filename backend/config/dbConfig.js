668b5cfa8aa38e271a902693c51dfcea9a19871b
thao <nguyen.thi.ngoc.thao@sun-asterisk.com>
2025-05-19 15:59:16 +0700

    Refix connect db cloud

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -3,12 +3,24 @@ const { Sequelize } = require("sequelize");
 const config = require("./config");
 const env = process.env.NODE_ENV || "development";
 const dbConfig = config[env];
-module.exports = new Sequelize(
-  dbConfig.database,
-  dbConfig.username,
-  dbConfig.password,
-  {
-    host: dbConfig.host,
+
+let sequelize;
+
+if (dbConfig.use_env_variable) {
+  sequelize = new Sequelize(process.env[dbConfig.use_env_variable], {
     dialect: dbConfig.dialect,
-  }
-);
+    dialectOptions: dbConfig.dialectOptions,
+  });
+} else {
+  sequelize = new Sequelize(
+    dbConfig.database,
+    dbConfig.username,
+    dbConfig.password,
+    {
+      host: dbConfig.host,
+      dialect: dbConfig.dialect,
+    }
+  );
+}
+
+module.exports = sequelize;
